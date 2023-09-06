4292a90a1555df4d4cd7e7e61ffe5f4a17392a8a
Claudivan Barreto da Silva <claudivanbarretodasilva@gmail.com>
2023-09-05 21:33:10 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import express, { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.use(route);
+
+app.listen(3001, () => 'Server running on port 3001');
