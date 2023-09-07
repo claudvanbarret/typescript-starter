e56e2144be81f8048de174235cb799f6d0237d18
Claudivan Barreto da Silva <claudivanbarretodasilva@gmail.com>
2023-09-06 23:12:47 -0300

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Router, Request, Response } from 'express';
+import cors from 'cors';
 
 import './utils/paths';
 
@@ -9,6 +10,7 @@ const app = express();
 const route = Router();
 
 app.use(express.json());
+app.use(cors());
 
 route.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
